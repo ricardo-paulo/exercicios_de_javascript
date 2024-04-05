6a85d3034472ecb9925dbc7ea2773de8bcdcb181
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2024-04-05 14:44:54 -0300

    Começo da atividade

---
+++ script.js
@@ -0,0 +1,31 @@
+// Questão 1
+
+function fatorial (number) {
+
+    let res = number
+
+    for (let i = 1; i < number; i++) {
+
+        res *= i
+
+    }
+
+    return res
+
+}
+
+function getCombVal () {
+
+    n = Number(prompt("Insira o valor: "))
+    k 
+
+}
+
+function combinacao () {
+
+    param = getCombVal()
+
+    console.log(`${n} e ${k}`)
+
+    document.getElementById('res-1').innerHTML = fatorial(3)
+}
