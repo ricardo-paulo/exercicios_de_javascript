f74adc4f8da7a7880a32f8cdfdef219927887fe5
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2024-04-08 17:55:15 -0300

    pequenas alterações: no meio da dois

--- script.js
+++ script.js
@@ -14,18 +14,44 @@ function fatorial (number) {
 
 }
 
-function getCombVal () {
+function combinacao () {
 
-    n = Number(prompt("Insira o valor: "))
-    k 
+    n = Number(prompt("Insira o valor N: "))
+    k = Number(prompt("Insira o valor K: "))
+
+    const result = fatorial(n)/fatorial(n-k) * fatorial(k)
+
+    console.log(`${n} e ${k} = ${result}`)
+
+    document.getElementById('res-1').innerHTML = result
+}
+
+// Questão 2
+
+let stopRot = false
+
+function rotate () {
+
+    while (true) {
+
+        setTimeout(() => {
+
+            baseStr = document.getElementById('res-2').innerHTML
+            finalStr = baseStr.substring(baseStr.length - 1).concat(baseStr.substring(0, baseStr.length - 1))
+            console.log(baseStr.length)
+            
+            document.getElementById('res-2').innerHTML = finalStr
+
+        }, 1500)
+
+        console.log('feito!')
+
+    }
 
 }
 
-function combinacao () {
+function stopRotate () {
 
-    param = getCombVal()
+    stopRotate = true
 
-    console.log(`${n} e ${k}`)
-
-    document.getElementById('res-1').innerHTML = fatorial(3)
 }
